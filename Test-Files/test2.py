b1fd7595dedaa682d699695133462ed104430f3d
ZtotheZ <34552429+ztothez@users.noreply.github.com>
2025-04-14 15:45:11 +0300

    Delete Test-Files directory

--- Test-Files/test2.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-p = subprocess.Popen(["ps", "-a"], stdout=subprocess.PIPE)
-out, err = p.communicate()
-if ('network-manager' in str(out)):
-    print('Httpd running')
-if ('mysql' in str(out)):
-    print('mysql running')
--- Test-Files/test3.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-output = subprocess.check_output(['ps', '-A'])
-if 'Network-Manager' in output:
-    print("Httpd is up an running!")
